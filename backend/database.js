45db19b7c9e69fed9bdcf9d3cd8f53f4a38e3924
YuliaNevar <yuli032005@gmail.com>
2025-12-17 12:01:09 +0200

    Made more adjustments

--- backend/database.js
+++ backend/database.js
@@ -1,14 +1,19 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('d26893_busstops', 'd26893_busstops', '3w7PYquFJhver0!KdOfF', {
-    host: 'd26893.mysql.zonevs.eu',
+const sequelize = new Sequelize(
+  process.env.DB_NAME || 'd26893_busstops',
+  process.env.DB_USER || 'd26893_busstops',
+  process.env.DB_PASSWORD || '3w7PYquFJhver0!KdOfF',
+  {
+    host: process.env.DB_HOST || 'd26893.mysql.zonevs.eu',
     dialect: 'mysql',
     port: 3306,
-    logging: false // отключает лишние логи
-});
+    logging: false
+  }
+);
 
 sequelize.authenticate()
-    .then(() => console.log('Connection to database is successfull!'))
-    .catch(err => console.error('Error of the connection: ', err));
+  .then(() => console.log('Connection to database is successfull!'))
+  .catch(err => console.error('Error of the connection: ', err));
 
 module.exports = sequelize;
