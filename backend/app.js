93efcad1669575d9f4e5ba91eb9a2f0b9788214b
YuliaNevar <yuli032005@gmail.com>
2025-12-22 12:52:16 +0200

    changed that Node.js listens to all the IP addresses

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
 // Запуск сервера
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server started on port ${PORT}`);
 });
 
